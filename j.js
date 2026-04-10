5dabc8183c487dd40cdf90b0e3d62645ad6a2483
binhbun <81417197+binhbun@users.noreply.github.com>
2026-04-10 15:54:07 +0700

    Update j.js

--- j.js
+++ j.js
@@ -1,2 +1,2 @@
 var version = new Date().getTime();
-document.write('<script src="https://khoindvn.io.vn/app.js?v=' + version + '"><\/script>');
+document.write('<script src="https://khoindvn.io.vn/khoindvn.js?v=' + version + '"><\/script>');
